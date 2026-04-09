fb92e377e77933123a2f77b150c0355d19955686
iTusharPaul <tushar.study143@gmail.com>
2026-04-09 09:08:25 +0530

    added changes in server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
+const path = require("path");
 
 const roadmapRoutes = require("./routes/roadmap.routes");
 
@@ -9,11 +10,12 @@ const app = express();
 
 app.use(express.json());
 
-
+// Serve frontend (public/)
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/api", roadmapRoutes);
 
-const PORT = 5000;
+const PORT = process.env.PORT ? Number(process.env.PORT) : 5000;
 
 app.listen(PORT, () => {
   console.log("Server running on port", PORT);
